2eb5c045803fb92022c8606322013aec13d07b9e
Gugo888 <kroyangurgen@gmail.com>
2022-01-25 17:21:47 +0300

    initial

---
+++ src/app/app.component.ts
@@ -0,0 +1,35 @@
+import { Component } from '@angular/core';
+
+type User = {name:string, visible:boolean}
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+
+export class AppComponent {
+
+  users:User[] = [{name:'Gugo', visible:true},
+  {name:'Rafo', visible:true},
+  {name:'Hakob', visible:true},
+  {name:'Saqo', visible:true},
+  ]
+
+  add(UserName:any) {
+    this.users.push({name:UserName.value, visible:true})
+
+    UserName.value = ''
+  }
+
+  delete(num:number) {
+    if(this.users.length > 1) {
+
+      this.users.splice(num,1);
+    } 
+  }
+
+  hide(param:User) {
+    param.visible = param.visible === true ? false:true
+  }
+}
